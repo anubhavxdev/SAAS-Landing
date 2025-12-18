c1645c61aa3dd49832c19a7217a81122e88d249b
Anubhav Jaiswal <anubhav.jaiswal23@lpu.in>
2025-12-18 16:07:47 +0530

    push 1

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './index.css'
+import Hero from "../src/components/mvpblocks/gradient-hero";
+import Features from "../src/components/mvpblocks/feature-2";
+import Pricing from "../src/components/mvpblocks/designer-pricing";
+
+
+const App = () => {
+  return (
+    <div>
+      <Hero />
+      <Features />
+      <Pricing />
+      
+    </div>
+  )
+}
+
+export default App
